609e136bb9b289d25f1a0565529d0b1b92bf0ab5
Angelo Lim <angelovinolim@gmail.com>
2018-09-10 18:07:00 -0400

    My name is Jeff.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+class Tester {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String args[]) {
+
+        Scanner kbInput = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String firstName = kbInput.next();
+        System.out.println("What is your last name?");
+        String lastName = kbInput.next();
+        System.out.println("Your full name is " + firstName + " " + lastName);
+
     }
 }
